72aaffa5af4c65723e9cea407f6440daf4e8c745
gippo05 <gsarinas17@gmail.com>
2025-08-18 09:54:46 +0800

    added extension

--- back-end/routes/orderRoutes.js
+++ back-end/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const createOrderRouter = express.Router();
-import { createOrder } from '../controllers/createOrder';
+import { createOrder } from '../controllers/createOrder.js';
 
 // import function from ordercontroller
 
