bb86ce00b1f6dc5daabf16eb81abab565b6a510a
gippo05 <gsarinas17@gmail.com>
2025-08-20 22:27:43 +0800

    modified controller to support pagination of products

--- back-end/controllers/fetchOrders.js
+++ back-end/controllers/fetchOrders.js
@@ -3,9 +3,20 @@ import Order from "../models/orderSchema.js";
 
 export const getOrders = async (req, res) =>{
     try{
-        const orders = await Order.find({});
-        res.json(orders);
-    } catch(error){
-        res.status(500).json({message: 'Failed to fetch products'})
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const skip = (page - 1) * limit;
+
+        const orders = await Order.find().skip(skip).limit(limit);
+        const total = await Order.countDocuments();
+
+        res.json({
+            orders,
+            total,
+            page,
+            totalPages: Math.ceil(total / limit),
+        });
+    } catch (error){
+        res.status(500).json({ message: "Server Error: "});
     }
 }
