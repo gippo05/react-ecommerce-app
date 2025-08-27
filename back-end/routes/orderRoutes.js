a5441308255a203470724a55568a6e4c8fcc1a7a
Gian Sarinas <gsarinas17@gmail.com>
2025-08-27 23:34:52 +0800

    Delete back-end directory
    migrated backend to a different repository

--- back-end/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const createOrderRouter = express.Router();
-import { createOrder } from '../controllers/createOrder.js';
-
-// import function from ordercontroller
-
-createOrderRouter.post("/", createOrder);
-
-export default createOrderRouter;
--- back-end/routes/viewOrderRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getOrders } from '../controllers/fetchOrders.js';
-
-const fetchOrderRouter = express.Router()
-
-
-fetchOrderRouter.get("/", getOrders);
-
-export default fetchOrderRouter;
